a86d8170767c9edce19023ade7f7c20344a6162e
daitech20 <daindp.tech@gmail.com>
2022-12-26 11:42:46 +0700

    pdai

--- api/models.py
+++ api/models.py
@@ -4,5 +4,4 @@ from django.db import models
 # Create your models here.
 class CountVehicles(models.Model):
     video = models.FileField(upload_to='videos/', max_length=255, blank=True)
-    video_output = models.FileField(upload_to='videos/', max_length=255, blank=True)
-    count = models.IntegerField(default=0)
+    video_output = models.FileField(upload_to='output/', max_length=255, blank=True)
--- api/serializer.py
+++ api/serializer.py
@@ -1,24 +1,18 @@
 from rest_framework import serializers
 from .models import CountVehicles
-from .core import count_vehicles
+from .main import detect
 
 class CountVehiclesSerializer(serializers.ModelSerializer):
     class Meta:
         model = CountVehicles
-        fields = ('video', 'video_output', 'count', )
-        extra_kwargs = {
-            'count': {
-                'required': False
-            }
-        }
+        fields = ('video', 'video_output', )
 
     def create(self, validated_data):
         video_vehicle = CountVehicles.objects.create(video=validated_data['video'])
 
         ## tinh toan tra lai ket qua
 
-        soluong = count_vehicles(video_vehicle.video, video_vehicle)
-        video_vehicle.count = soluong
-        video_vehicle.save()
+        vehicle = detect(video_vehicle)
+        # vehicle.save()
 
         return video_vehicle
